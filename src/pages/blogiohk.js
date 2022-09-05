847f89287e52b42169fab7fd92fcb092b8df500f
Nguyen Van Hieu <nvhieu1978@gmail.com>
2022-09-05 13:52:45 +0700

    update News

--- src/pages/blogiohk.js
+++ src/pages/blogiohk.js
@@ -1,137 +1,98 @@
-import React, { useEffect } from 'react';
-import Layout from '@theme/Layout';
-import Blogiohk from '../components/blog-iohk';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
-function HomepageHeader() {
-    useEffect(() => {
-        const canvas = document.getElementById('canvas');
-        const ctx = canvas.getContext('2d');
-    
-        canvas.width = window.innerWidth;
-        canvas.height = 204;
-    
-        // Array that contains the stars
-        const stars = [];
-    
-        // Frames per second
-        const FPS = 60;
-    
-        // Number of stars
-        const x = 100;
-    
-        // mouse location
-        const mouse = {
-          x: 0,
-          y: 0
-        };  
-    
-        // Push stars to array
-        for (var i = 0; i < x; i++) {
-            stars.push({
-                x: Math.random() * canvas.width,
-                y: Math.random() * canvas.height,
-                radius: Math.random() * 1 + 1,
-                vx: Math.floor(Math.random() * 50) - 25,
-                vy: Math.floor(Math.random() * 50) - 25
-            });
-        }
-    
-        // Draw the scene
-        function draw() {
-            ctx.clearRect(0,0,canvas.width,canvas.height);
-          
-            ctx.globalCompositeOperation = "lighter";
-          
-            for (var i = 0, x = stars.length; i < x; i++) {
-                var s = stars[i];
-          
-                ctx.fillStyle = "#fff";
-                ctx.beginPath();
-                ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-                ctx.fill();
-                ctx.fillStyle = 'black';
-                ctx.stroke();
-            }
-          
-            ctx.beginPath();
-            for (var i = 0, x = stars.length; i < x; i++) {
-                var starI = stars[i];
-                ctx.moveTo(starI.x,starI.y); 
-                if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
-                for (var j = 0, x = stars.length; j < x; j++) {
-                    var starII = stars[j];
-                    if(distance(starI, starII) < 150) {
-                        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
-                        ctx.lineTo(starII.x,starII.y); 
-                    }
-                }
-            }
-            ctx.lineWidth = 0.05;
-            ctx.strokeStyle = 'white';
-            ctx.stroke();
-        }
-    
-        function distance( point1, point2 ){
-            var xs = 0;
-            var ys = 0;
-        
-            xs = point2.x - point1.x;
-            xs = xs * xs;
-        
-            ys = point2.y - point1.y;
-            ys = ys * ys;
-        
-            return Math.sqrt( xs + ys );
-        }
-    
-        // Update star locations
-        function update() {
-            for (var i = 0, x = stars.length; i < x; i++) {
-                var s = stars[i];
-          
-                s.x += s.vx / FPS;
-                s.y += s.vy / FPS;
-            
-                if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-                if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-            }
-        }
-    
-        canvas.addEventListener('mousemove', function(e){
-            mouse.x = e.clientX;
-            mouse.y = e.clientY;
-        });
-    
-        // Update and draw
-        function tick() {
-            draw();
-            update();
-            requestAnimationFrame(tick);
-        }
-    
-        tick();
-    }, [])
 
-    return (
-        <header style={{position: 'relative', width: '100%', overflow: 'hidden'}}>
-            <canvas id='canvas' style={{background: 'linear-gradient(to right, #000428, #004e92)', position: 'relative'}}></canvas>
-            <div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: 'white'}}>
-                <h1 className="hero__title">BLOG IOHK</h1>
+const features = [
+  {
+    title: "Những gì bạn cần biết Layer 1 và Layer 2",
+    imageUrl: "/docs1/2022/08/img/2022-08-05-layer-1-layer-2-all-you-need-to-know.007.jpeg",
+    targetUrl: "/docs1/2022/08/2022-08-05-layer-1-layer-2-all-you-need-to-know",
+    description: (
+      <>
+        Khi thảo luận về kiến trúc blockchain, các thuật ngữ 'Layer 1' và 'Layer 2' thường được đề cập. Đây là những khái niệm quan trọng phục vụ hai mục đích: giải thích cách mạng lưới blockchain được xây dựng và cung cấp hình ảnh trực quan dễ hiểu về mạng lưới blockchain trông như thế nào.
+      </>
+    ),
+  },
+  {
+    title: "Kachina: hợp đồng thông minh bảo vệ quyền riêng tư",
+    imageUrl: "/docs1/2022/08/img/2022-08-05-kachina-privacy-preserving-smart-contracts.007.jpeg",
+    targetUrl: "/docs1/2022/08/2022-08-05-kachina-privacy-preserving-smart-contracts",
+    description: (
+      <>
+        Tận dụng ZK Snarks để kích hoạt chức năng hợp đồng thông minh bảo vệ quyền riêng tư mà không hy sinh các đặc điểm phi tập trung.
+      </>
+    ),
+  },
+  {
+    title: "Giới thiệu Ofelimos : giao thức đồng thuận bằng chứng công việc hữu ích",
+    imageUrl: "/docs1/2022/08/img/2022-08-16-introducing-ofelimos-a-proof-of-useful-work-consensus-protocol.007.png",
+    targetUrl: "/docs1/2022/08/2022-08-16-introducing-ofelimos-a-proof-of-useful-work-consensus-protocol",
+    description: (
+      <>
+        Nghiên cứu của IOG giới thiệu một giao thức đồng thuận mới, an toàn có thể chứng minh được để giảm thiểu sự lãng phí năng lượng của các blockchains bằng chứng công việc.
+      </>
+    ),
+  },
+  
+    
+];
+
+function Feature({ imageUrl, title, description, targetUrl }) {
+  const imgUrl = useBaseUrl(imageUrl); // not used right now
+  const trgUrl = useBaseUrl(targetUrl);
+  return (
+    <div className={clsx("col col--4", styles.featurePadding)}>
+      {targetUrl && (
+        <Link className="navbar__link" to={trgUrl}>
+          <div className="card">
+            <div className="card__header">
+              {imgUrl && (
+                <div className="text--center">
+                  <img
+                    className={styles.featureImage}
+                    src={imgUrl}
+                    alt={title}
+                  />
+                </div>
+              )}
+              <h3>{title}</h3>
             </div>
-        </header>
-    );
+            <div className="card__body">
+              <p>{description}</p>
+            </div>
+          </div>
+        </Link>
+      )}
+    </div>
+  );
 }
 
-export default function Home() {
-    return (
-        <Layout
-            title={`Blog`}
-            description="Mang Cardano về Việt Nam">
-            <HomepageHeader />
-            <main>
-                <Blogiohk />
-            </main>
-        </Layout>
-    );
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout description="Cardano Developer Portal">
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+
+    </Layout>
+  );
 }
+
+export default Home;
